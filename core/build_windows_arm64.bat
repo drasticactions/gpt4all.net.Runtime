a7aa0754f709e5bdf114ad74913ba30a30fbfe86
Tim Miller <innerlogic4321@gmail.com>
2023-05-30 12:56:33 +0900

    Windows

--- core/build_windows_arm64.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set BUILD_TYPE=Release
-
-:windows_arm64
-cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -S . -B build\win-arm64 -A ARM64 -DCMAKE_SYSTEM_PROCESSOR=aarch64
-cmake --build build\win-arm64 --config %BUILD_TYPE%
-mkdir gpt4all.net.Runtime/win-arm64
-copy build/win-arm64/bin/Release/libllmodel.dll gpt4all.net.Runtime/win-arm64/libllmodel.dll
--- core/build_windows_x64.bat
+++ core/build_windows_x64.bat
@@ -4,6 +4,7 @@ set BUILD_TYPE=Release
 
 :windows_x64
 cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -S . -B build\win-x64 -A x64
-cmake --build build\win-x64 --config %BUILD_TYPE%
+cmake --build build\win-x64 --parallel --config %BUILD_TYPE%
 mkdir gpt4all.net.Runtime/win-x64
-copy build/win-x64/bin/Release/libllmodel.dll gpt4all.net.Runtime/win-x64/libllmodel.dll
+copy build/win-x64/bin/Release/llama.dll gpt4all.net.Runtime/win-x64/llama.dll
+copy build/win-x64/gpt4all/Release/libllmodel.dll gpt4all.net.Runtime/win-x64/libllmodel.dll

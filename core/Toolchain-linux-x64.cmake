c2ebea7b6d9433ba2d63032e1da64b217e8f5f5b
Tim Miller <innerlogic4321@gmail.com>
2023-06-10 22:50:35 +0900

    Update

--- core/Toolchain-linux-x64.cmake
+++ core/Toolchain-linux-x64.cmake
@@ -1,9 +1,9 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-set(CMAKE_C_COMPILER "x86_64-linux-musl-gcc")
-set(CMAKE_CXX_COMPILER "x86_64-linux-musl-g++")
-set(CMAKE_FIND_ROOT_PATH "/usr/local/x86_64-linux-musl")
+set(CMAKE_C_COMPILER "/usr/bin/gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/g++")
+# set(CMAKE_FIND_ROOT_PATH "/usr/local/x86_64-linux-musl")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
